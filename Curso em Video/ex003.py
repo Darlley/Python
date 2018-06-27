61aa46076c692347aeddccb3234e82f04a91153a
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-27 13:29:48 -0400

    Create ex003.py

---
+++ Curso em Video/ex003.py
@@ -0,0 +1,7 @@
+n1 = input('Valor 1: ')
+n2 = input('Valor 2: ')
+s = int(n1) + int(n2)
+
+#print('A soma é igual a: ',s)
+#print('A soma entre',n1,' e ',n2' é igual a: ',s)
+print('A soma de {} e {} é igual a: {}'.format(n1, n2, s))
