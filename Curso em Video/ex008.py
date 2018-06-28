4745ab8362f8a36c9dff082e5427332f353a489d
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-28 16:00:15 -0400

    Create ex008.py

---
+++ Curso em Video/ex008.py
@@ -0,0 +1,5 @@
+valor = float(input('Quantos metros quer converter? '))
+centimetros = v * 100
+milimetros = v * 1000
+
+print('{}m equivale a {:.0f}cm e {:.0f}mm'.format(valor, centimetros, milimetros))
