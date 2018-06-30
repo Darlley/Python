d5d0ecbe1d256ad1996d08696a3f9195dab9bddf
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-30 14:47:22 -0400

    Create ex016.py

---
+++ Curso em Video/ex016.py
@@ -0,0 +1,4 @@
+from math import trunc
+numeroReal = float(input('Digite um número do tipo real: '))
+numeroInteiro = trunc(nr)
+print('O numero real {} inteiro vale {}'.format(numeroReal, numeroInteiro))
