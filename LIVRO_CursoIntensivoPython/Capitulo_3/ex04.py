b9c1502c754147cdf378cde3c0c6ab8bb262627b
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-21 14:42:37 -0400

    Create ex04.py

---
+++ LIVRO_CursoIntensivoPython/Capitulo_3/ex04.py
@@ -0,0 +1,11 @@
+#3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+#jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas
+#que você gostaria de convidar para jantar. Em seguida, utilize sua lista para exibir
+#uma mensagem para cada pessoa, convidando-a para jantar.
+
+convidados = ['Alvin Plantinga', 'William Lane Craig', 'Alister McGrath']
+mensagem_saudação = "Olá senhor "
+mensagem_convite = ".\nGostaria muito de sua presença em meu jantar.\n"
+print(mensagem_saudação+convidados[0].title()+mensagem_convite)
+print(mensagem_saudação+convidados[1].title()+mensagem_convite)
+print(mensagem_saudação+convidados[2].title()+mensagem_convite)
