2744d5e0d36127655423c68bf28b3d2ec538ec64
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-21 14:35:24 -0400

    Create ex06.py

---
+++ LIVRO_CursoIntensivoPython/Capitulo_2/ex06.py
@@ -0,0 +1,3 @@
+famous_person = "Friedrich Nietzsche" 
+message = '"A sabedoria é um paradoxo.\nO homem que mais sabe é aquele que mais reconhece\na vastidão da sua ignorância"'
+print(famous_person + " disse:\n" +message)
