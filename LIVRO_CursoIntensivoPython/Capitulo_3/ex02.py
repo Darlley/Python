cb779921997a840956d6f62c5bfed3140991c53c
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-21 14:40:29 -0400

    Create ex02.py

---
+++ LIVRO_CursoIntensivoPython/Capitulo_3/ex02.py
@@ -0,0 +1,11 @@
+#3.2 – Saudações: Comece com a lista usada no Exercício 3.1, mas em vez de
+#simplesmente exibir o nome de cada pessoa, apresente uma mensagem a elas. O
+#texto de cada mensagem deve ser o mesmo, porém cada mensagem deve estar
+#personalizada com o nome da pessoa.
+
+nome = ['Lucas', 'Fernanda', 'Leticia', "Felipe"]
+mensgaem = "Este é meu/minha amigo(a) chamado(a) "
+print(mensgaem + nome[0].title())
+print(mensgaem + nome[1].title())
+print(mensgaem + nome[2].title())
+print(mensgaem + nome[3].title())
