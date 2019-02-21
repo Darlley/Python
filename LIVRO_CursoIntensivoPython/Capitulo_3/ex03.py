33629a918e3af908a2465706893ef3008715cf4e
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-21 14:41:39 -0400

    Create ex03.py

---
+++ LIVRO_CursoIntensivoPython/Capitulo_3/ex03.py
@@ -0,0 +1,11 @@
+#3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+#jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas
+#que você gostaria de convidar para jantar. Em seguida, utilize sua lista para exibir
+#uma mensagem para cada pessoa, convidando-a para jantar.
+
+nome = ["saveiro", "honda civic", "camaro", "lamborguini"]
+mensgaem = "Sonho de consumo este(a) "
+print(mensgaem + nome[0].title())
+print(mensgaem + nome[1].title())
+print(mensgaem + nome[2].title())
+print(mensgaem + nome[3].title())
