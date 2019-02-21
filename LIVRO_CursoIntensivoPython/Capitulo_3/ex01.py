1465ef9a77f027cba6add201839a2ce088edb85b
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-21 14:39:50 -0400

    Create ex01.py

---
+++ LIVRO_CursoIntensivoPython/Capitulo_3/ex01.py
@@ -0,0 +1,9 @@
+#3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
+#chamada names. Exiba o nome de cada pessoa acessando cada elemento da lista,
+#um de cada vez.
+
+nome = ['lucas', 'fernanda', 'leticia', "felipe"]
+print(nome[0].title())
+print(nome[1].title())
+print(nome[2].title())
+print(nome[3].title())
